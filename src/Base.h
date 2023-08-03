c9b5733dfb39df3a76c27777283bbbc454d59909
Henriquelmeeee <smtpwebhost@gmail.com>
2023-08-02 21:07:02 -0300

    Adicionando comentários e diretivas relacionadas ao registrador FLAGS, para uso futuro

--- src/Base.h
+++ src/Base.h
@@ -22,6 +22,37 @@ struct Registers {
     unsigned short ir;
 } extern regs;
 
+/* FLAGS
+ bits
+      0->CF->Carry Flag
+      1->Reserved
+      2->PF->Parity Flag
+      3->Reserved
+      4->AF->Auxiliary Carry Flag
+      5->Reserved
+      6->Zero Flag
+      7->Sign Flag
+      8->Trap Flag (debugging purposes)
+      9->IF->Interrupt Flag
+      10->DF->Direction Flag
+      11->OF->Overflow Flag
+      12,13,14,15->IOPL->I/O Privilege Level Field
+
+      NOW, x86-32BITS FLAGS RELATED, NOT RELEVANT IN THIS CASE!!!!!!!!!!!!!
+      16->NT->Nested Task Flag
+      17->Reserved
+      18->RF->Resume Flag (debugging purposes)
+      19->VM->Virtual 8086 mode
+      20->AC->Allignment Check
+      21->VIF->Virtual Interrupt Flag
+      22->VIP->Virtual Interrupt Pending
+      23->ID->ID flag
+      24,25,26,27,28,29,30,31->Reserved
+*/
+
+#define CF ( (regs.flags)&1 )
+#define ZF ( (regs.flags)&128 )
+
 enum ExecutionState {
     SUCCESS,
     ERROR,
